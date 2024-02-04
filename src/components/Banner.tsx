3ff18579ea6d07fda56ef38b2c58a54771c5a236
Uncle Pedro <you@example.com>
2024-02-05 08:43:07 +1000

    Fit background image to screen

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 const Banner = () => {
   return (
     <>
-      <div className="flex justify-center items-center bg-yellow-300 p-4">
+      <div className="flex justify-center items-center bg-rose-900 text-white p-4">
         <h1 className="text-5xl font-oxygen font-bold">Play-This ▶️</h1>
       </div>
     </>
