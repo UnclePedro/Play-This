c15fe21943b551ff8f331a4db8f3a7b950665bbd
Uncle Pedro <you@example.com>
2024-02-05 09:19:17 +1000

    Remove PageWrapper from code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 // import { useState } from 'react';
 import Banner from './components/Banner';
-import PageWrapper from './components/PageWrapper';
 import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="h-screen scale-down">
-      <PageWrapper />
       <Banner />
       <SearchBar />
     </div>
