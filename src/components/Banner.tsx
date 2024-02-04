fad98c324f15c62435cdcf3530d77601fa05663c
Uncle Pedro <peterforsyth10@gmail.com>
2024-02-04 11:07:47 +1000

    Import custom font, style banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,9 @@
 const Banner = () => {
   return (
     <>
-      <h1 className="font-xl">Play-This!</h1>
+      <div className="flex justify-center items-center bg-yellow-300 p-4">
+        <h1 className="text-5xl font-oxygen font-bold">Play-This</h1>
+      </div>
     </>
   );
 };
