b32c9e01b5dc607bbd06088203dee9c25ecd6d4d
Uncle Pedro <peterforsyth10@gmail.com>
2024-02-05 09:19:17 +1000

    Remove PageWrapper from code

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,7 +3,7 @@ const SearchBar = () => {
     <>
       <div className="flex justify-center items-center font-oxygen p-4">
         <label>
-          <input className="border-yellow-300 border-2 rounded m-4 p-1"></input>
+          <input className="border-2 rounded m-4 p-1"></input>
         </label>
         <button className="text-white bg-gray-600 px-2 py-1 rounded">Jam!</button>
       </div>
