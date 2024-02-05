46cfdbcb334a6ff2615571752abdc3612a2aa762
Uncle Pedro <peterforsyth10@gmail.com>
2024-02-05 11:51:48 +1000

    Start building function to map results into list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react';
 import Banner from './components/Banner';
 import SearchBar from './components/SearchBar';
+import SearchResults from './components/SearchResults';
 
 function App() {
   return (
-    <div className="h-screen scale-down">
+    <div className="h-screen bg-rose-900/75">
       <Banner />
       <SearchBar />
+      <SearchResults />
     </div>
   );
 }
