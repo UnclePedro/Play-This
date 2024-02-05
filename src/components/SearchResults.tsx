82fc9bc64bb596c345cda46aa9c3c5ee3cb8a638
Uncle Pedro <peterforsyth10@gmail.com>
2024-02-05 13:19:44 +1000

    Song results styling

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -15,19 +15,24 @@ const SearchResults = () => {
 
   return (
     <>
-      <div className="justify-center p-8 text-white bg-pink-900 w-[400px]">
-        <h1 className="text-5xl font-oxygen font-bold p-3">Results:</h1>
+      <div className="p-8 text-white bg-pink-900 w-[400px] rounded-xl shadow-xl ">
+        <h2 className="text-4xl font-oxygen font-bold p-3">Results:</h2>
         <ul>
           {songs.map((song) => (
             <>
-              <li key={song.songName} className="py-3 bg-pink-700 my-4 flex flex-row space-x-10 p-4">
-                <div>
-                  <p>{song.songName}</p>
-                  <p>{song.songArtist}</p>
-                  <p>{song.songLength}</p>
-                </div>
-                <button className="bg-black h-fit w-6 rounded-full font-bold">+</button>
-              </li>
+              <button>
+                <li
+                  key={song.songName}
+                  className="py-3 w-72 bg-pink-700 my-4 flex flex-row space-x-10 p-4 hover:bg-violet-600 rounded-xl shadow-lg transition-all duration-300"
+                >
+                  <div className="text-left">
+                    <h3 className="font-bold text-lg">{song.songName}</h3>
+                    <p>{song.songArtist}</p>
+                    <p>{song.songLength}</p>
+                  </div>
+                  <p className="font-bold">+</p>
+                </li>
+              </button>
             </>
           ))}
         </ul>
