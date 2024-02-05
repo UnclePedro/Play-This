481a9ecea9fff2eec66f063fdc09dc9c598a2a79
Uncle Pedro <you@example.com>
2024-02-05 13:25:21 +1000

    Positioning of SearchResults

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -15,27 +15,29 @@ const SearchResults = () => {
 
   return (
     <>
-      <div className="p-8 text-white bg-pink-900 w-[400px] rounded-xl shadow-xl ">
-        <h2 className="text-4xl font-oxygen font-bold p-3">Results:</h2>
-        <ul>
-          {songs.map((song) => (
-            <>
-              <button>
-                <li
-                  key={song.songName}
-                  className="py-3 w-72 bg-pink-700 my-4 flex flex-row space-x-10 p-4 hover:bg-violet-600 rounded-xl shadow-lg transition-all duration-300"
-                >
-                  <div className="text-left">
-                    <h3 className="font-bold text-lg">{song.songName}</h3>
-                    <p>{song.songArtist}</p>
-                    <p>{song.songLength}</p>
-                  </div>
-                  <p className="font-bold">+</p>
-                </li>
-              </button>
-            </>
-          ))}
-        </ul>
+      <div className="flex justify-center items-center my-8">
+        <div className="p-8 text-white bg-pink-900 w-[400px] rounded-xl shadow-xl">
+          <h2 className="text-4xl font-oxygen font-bold p-3">Results:</h2>
+          <ul>
+            {songs.map((song) => (
+              <>
+                <button>
+                  <li
+                    key={song.songName}
+                    className="py-3 w-72 bg-pink-700 my-4 flex flex-row space-x-10 p-4 hover:bg-violet-600 rounded-xl shadow-lg transition-all duration-300"
+                  >
+                    <div className="text-left">
+                      <h3 className="font-bold text-lg">{song.songName}</h3>
+                      <p>{song.songArtist}</p>
+                      <p>{song.songLength}</p>
+                    </div>
+                    <p className="font-bold">+</p>
+                  </li>
+                </button>
+              </>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
