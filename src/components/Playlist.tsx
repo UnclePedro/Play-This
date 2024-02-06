5c5bc8e119a54e66c176c8b22d52229a5364ec2e
Uncle Pedro <you@example.com>
2024-02-06 11:30:55 +1000

    Separate SearchResults and Playlist components - Made SearchResults pass playlist data to Playlist component - Create playlist name input - Add save button - Change background

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,26 +1,24 @@
-const Playlist = () => {
-  const playlist = [
-    // Receive song objects array from search component, and map into a results list
-    {
-      name: 'We Run',
-      artist: 'Bailey Ibbs',
-      length: '6:11',
-    },
-    {
-      name: 'Gas Me Up (Diligent)',
-      artist: 'Skepta',
-      length: '2:51',
-    },
-  ];
-
+const Playlist = (props: { playlistData: object[] }) => {
   return (
     <>
-      <div className="flex justify-center items-center my-8">
-        <div className="flex justify-center items-center font-oxygen bg-pink-900 w-fit rounded-lg">
-          <input className="border-2 rounded-xl m-4 p-3 text-center" placeholder="Name your playlist..."></input>
-          {playlist[0].name}
-        </div>
-      </div>
+      <input placeholder="Playlist..." className="text-4xl font-oxygen font-bold p-3 rounded-xl w-72"></input>
+      <ul>
+        {props.playlistData.map((playlistItem: object) => (
+          <li
+            key={playlistItem.name}
+            className="py-3 w-72 bg-pink-700 my-2 flex flex-row space-x-10 p-4 hover:bg-violet-600 rounded-xl shadow-lg transition-all duration-300 text-white"
+          >
+            <div className="text-left">
+              <h3 className="font-bold text-lg">{playlistItem.name}</h3>
+              <p>{playlistItem.artist}</p>
+              <p>{playlistItem.length}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <button className="py-3 w-72 bg-white font-bold my-2 flex flex-row space-x-10 p-4 hover:bg-violet-600 rounded-xl shadow-lg transition-all duration-300 ">
+        Save to Spotify
+      </button>
     </>
   );
 };
