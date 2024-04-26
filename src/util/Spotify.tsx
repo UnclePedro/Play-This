dc5ea61fe9d8832a24ce089fd7b68e80b2a29f6d
Uncle Pedro <you@example.com>
2024-04-26 17:15:15 +1000

    Console logging and debugging

--- src/util/Spotify.tsx
+++ src/util/Spotify.tsx
@@ -1,12 +1,14 @@
-const Spotify = () => {
-  let accessToken: any;
-  const clientId = '17b12d160569489584b96ea99cc989cb';
-  const redirectUrl = 'http://localhost:3000';
+let accessToken: any;
+const clientId = '17b12d160569489584b96ea99cc989cb';
+const redirectUrl = 'http://localhost:3000/';
 
+const Spotify = () => {
   const getAccessToken = () => {
     if (accessToken) return accessToken;
     const tokenInURL = window.location.href.match(/access_token=([^&]*)/);
     const expiryTime = window.location.href.match(/expires_in=([^&]*)/);
+    console.log(window.location.href);
+    console.log(tokenInURL);
 
     if (tokenInURL && expiryTime) {
       // set access token and expiry time variables
@@ -19,21 +21,30 @@ const Spotify = () => {
       // clear url after access token expires
       window.history.pushState('Access token', '/');
       return accessToken;
+    } else {
+      // Third check for access token if first and second are both false
+      const redirect = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUrl}`;
+      window.location.href = redirect;
     }
-    // Third check for access token if first and second are both false
-    const redirect = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUrl}`;
-    window.location.href = redirect;
   };
 
   const search = async (term: string) => {
-    // console.log(term);
+    console.log(term);
+
     accessToken = getAccessToken();
+    console.log(accessToken);
+
     const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
       method: 'GET',
       headers: { Authorization: `bearer ${accessToken}` },
     });
+    console.log(response);
     const jsonResponse = await response.json();
-    if (!jsonResponse) {
+
+    console.log(window.location.href);
+
+    if (jsonResponse) {
+      console.log(jsonResponse);
       return jsonResponse.tracks.items.map(
         (track: { id: string; name: string; artists: string; album: any; uri: string }) => ({
           id: track.id,
