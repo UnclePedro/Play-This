6da816a3ab9ecf0353aa0e2c1e36261e8e7729de
Uncle Pedro <you@example.com>
2024-05-02 13:01:43 +1000

    Add redirectUri correctly

--- src/util/Authorize.tsx
+++ src/util/Authorize.tsx
@@ -1,5 +1,5 @@
 const SPOTIFY_CLIENT_ID = '17b12d160569489584b96ea99cc989cb';
-const redirectUri = 'http://localhost:3000/auth/callback';
+const redirectUri = 'http://localhost:5173/auth/callback';
 
 function generateRandomString(length: number): string {
   let text = '';
