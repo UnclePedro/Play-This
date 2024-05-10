18b89809107be4996d460ac8dff420a1d5317f14
Uncle Pedro <you@example.com>
2024-05-10 12:28:57 +1000

    Successful debug of Search component

--- src/util/Search.tsx
+++ src/util/Search.tsx
@@ -2,7 +2,7 @@ export const search = async (term: string) => {
   const accessToken = sessionStorage.getItem('access_token');
   console.log(`Your access token is: ${accessToken}`);
 
-  const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+  const response = await fetch(`https://api.spotify.com/v1/search?type=track&limit=10&q=${term}`, {
     method: 'GET',
     headers: { Authorization: `Bearer ${accessToken}` },
   });
@@ -11,15 +11,13 @@ export const search = async (term: string) => {
   if (jsonResponse) {
     // console.log(jsonResponse);
     const results = jsonResponse.tracks.items.map(
-      (track: { id: string; name: string; artists: string; album: any; uri: string }) => [
-        {
-          id: track.id,
-          name: track.name,
-          artist: track.artists[0],
-          album: track.album.name,
-          uri: track.uri,
-        },
-      ],
+      (track: { id: string; name: string; artists: any; album: any; uri: string }) => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri,
+      }),
     );
     return results;
   }
