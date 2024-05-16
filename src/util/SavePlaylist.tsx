ae9a659434f59509aa6353183e412060027f649a
Uncle Pedro <peterforsyth10@gmail.com>
2024-05-16 18:17:33 +1000

    Handle no playlist tracks or name cases - Error messages for each scenario

--- src/util/SavePlaylist.tsx
+++ src/util/SavePlaylist.tsx
@@ -1,5 +1,20 @@
 export const savePlaylist = async (playlistName: string, trackURIs: any) => {
-  console.log(trackURIs);
+  // console.log(trackURIs);
+  console.log(trackURIs.length);
+  console.log(`Playlist name is ${playlistName}`);
+
+  // Return if no tracks are added to playlist, to avoid creating empty playlists
+  if (trackURIs.length === 0) {
+    window.alert('Cannot create empty playlist, add some tracks.');
+    return;
+  }
+
+  // Return if playlist is not named
+  if (playlistName.length === 0) {
+    window.alert('Please name your playlist first.');
+    return;
+  }
+
   const accessToken = sessionStorage.getItem('access_token');
 
   const getUserId = await fetch(`https://api.spotify.com/v1/me`, {
