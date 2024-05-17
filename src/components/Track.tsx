74e3df6adb2cbaff3fc71673ab78bb916c935ed8
Uncle Pedro <peterforsyth10@gmail.com>
2024-05-17 17:46:32 +1000

    Adjust formatting of tracklist and playlist

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -1,5 +1,5 @@
 const Track = (props: { isRemoval: boolean; onRemove: any; onAdd: any; track: any }) => {
-  console.log('Track:', props.track);
+  // console.log('Track:', props.track);
   const addTrack = () => {
     props.onAdd(props.track);
   };
@@ -27,14 +27,14 @@ const Track = (props: { isRemoval: boolean; onRemove: any; onAdd: any; track: an
     <>
       <div className="flex justify-center items-center ">
         <div className="w-11/12 sm:w-[500px] bg-pink-700 flex rounded-xl shadow-lg p-2 my-1">
-          <div className="flex items-center font-bold px-2 w-[200px]">
-            {props.track.name.length < 25 ? props.track.name : props.track.name.substring(0, 40) + '...'}
+          <div className="flex items-center font-bold px-2 w-1/3">
+            {props.track.name.length < 25 ? props.track.name : props.track.name.substring(0, 25) + '...'}
           </div>
-          <div className="flex items-center px-2 w-[166px]">
-            {props.track.artist.length < 25 ? props.track.artist : props.track.artist.substring(0, 40) + '...'}
+          <div className="flex items-center px-2 w-1/3">
+            {props.track.artist.length < 25 ? props.track.artist : props.track.artist.substring(0, 25) + '...'}
           </div>
-          <div className="flex items-center px-2 w-[166px]">
-            {props.track.album.length < 25 ? props.track.album : props.track.album.substring(0, 40) + '...'}
+          <div className="flex items-center px-2 w-1/3">
+            {props.track.album.length < 25 ? props.track.album : props.track.album.substring(0, 25) + '...'}
           </div>
 
           <div className="p-4">{renderAction()}</div>
