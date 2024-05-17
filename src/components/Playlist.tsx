f9749ab6314380d18834a1a5f7d10112c0d72f8a
Uncle Pedro <you@example.com>
2024-05-17 11:19:19 +1000

    Improve UI to limit string length displayed - Add guide for Name, Artist and Album

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -17,17 +17,17 @@ const Playlist = (props: {
     <>
       <div className="flex justify-center m-4">
         <div className="p-8 xl:my-8 text-white bg-pink-900 w-[600px] rounded-xl shadow-xl">
-          <div className="flex flex-col items-start">
+          <div className="flex flex-col">
             <input
               onChange={handleNameChange}
               // defaultValue={'New Playlist'}
               placeholder="New Playlist"
-              className="text-2xl font-oxygen font-bold p-3 rounded-xl w-72 text-black"
+              className="text-2xl font-oxygen font-bold p-3 rounded-xl w-72 text-black mb-1"
             />
             <Tracklist tracks={props.playlistTracks} isRemoval={true} onRemove={props.onRemove} onAdd={props.onAdd} />
             <button
               onClick={props.onSave}
-              className="py-3 w-72 bg-pink-700 font-bold my-2 flex flex-row space-x-10 p-4 hover:bg-violet-600 rounded-xl shadow-lg transition-all duration-300 "
+              className="flex w-40 justify-center font-bold my-2 p-4 bg-pink-700 hover:bg-violet-600 rounded-xl shadow-lg transition-all duration-300 "
             >
               Save to Spotify
             </button>
